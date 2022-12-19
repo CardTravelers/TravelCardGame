931df94646dfa589630f7997a54d76a10aaafafe
Mike Shen <michael.m.shen@gmail.com>
2022-12-19 21:35:33 +0000

    added base js file, images

--- js/app.js
+++ js/app.js
@@ -1,3 +1,84 @@
-'user strict'
+'use strict';
 
-console.log('hello world');
+// constructor to create location object
+// Array will have: name, hint1, hint2, option2, option3, option4, image reference
+function Locations (name, hint1, hint2, option2, option3, option4, imgPath) {
+
+
+
+}
+
+
+// Pick 10 random locations to display
+// Given list of location objects, pick 10 randomly
+// Return array with 10 locations
+function randomCard(numCards) {
+
+
+
+}
+
+
+// Show main image
+// Show 2 hints, blurred
+// Show 4 options with submit button
+// stretch goal: add audio
+function renderCard (){
+
+
+
+}
+
+
+
+// event listener for card
+function handleClick (event) {
+  // if click on hint 1, run another function
+  hintOneClick();
+
+  // if click on hint 2, run this function
+  hintTwoClick();
+
+  // if click on submit button, run this function
+  scoreAnswer();
+
+}
+
+
+// on click, unblur hint 1
+// decrement total possible score
+function hintOneClick(){
+
+}
+
+
+// on click, unblur hint 2
+// decrement total possible score
+function hintTwoClick(){
+
+}
+
+
+// calculate score for this card
+// save to totalscore
+// animation for correct / incorrect answers
+function scoreAnswer(){
+
+
+}
+
+
+// timer function.  Count down from 5 minutes (variable) until game end
+// When timer ends, end game, tally score
+function gameTimer(){
+
+
+
+}
+
+// high score table.  See if user score is in top 10.  If yes, add to table
+
+function highScore(){
+
+
+}
