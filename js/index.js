abc4a3ed08eb454f6b277d2fb2b38edf4aa933d6
Mike Shen <michael.m.shen@gmail.com>
2022-12-21 19:05:15 +0000

    file cleanup

--- js/index.js
+++ js/index.js
@@ -1,14 +1,22 @@
 'use strict';
 // ********* GLOBALS**********
+
 // **********DOM WINDOWS***********
-let userNameForm = document.getElementById('userNameForm');
+// let userNameForm = document.getElementById('userNameForm');
 let userName = document.getElementById('userName');
+let submitButton = document.getElementById('Send');
+
 // **********HELPER FUNCTION/UTILITIES**********
 function handleSubmit(event){
+  console.log(event);
   event.preventDefault();
   localStorage.setItem('userName', userName.value);
   window.location.href = 'gamepage.html';
 }
+
 // **********EVENT LISTENERS***************
-userNameForm.addEventListener('submit', handleSubmit);
+// userNameForm.addEventListener('submit', handleSubmit);
+submitButton.addEventListener('submit', handleSubmit);
+
+
 // *********** EXECUTABLE CODE**********
